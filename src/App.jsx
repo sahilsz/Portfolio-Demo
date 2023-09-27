e8fc25e943f813a63ee19ba2b82a523cdc6e1c95
darq <prototypid@gmail.com>
2023-09-27 19:20:46 +0530

    add id to links

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
       </section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
-      <section>Parallax</section>
+      <section id="Portfolio">Parallax</section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Portfolio</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
