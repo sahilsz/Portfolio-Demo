1356988ac703a95c1aa34ca26cda210f121807e8
darq <prototypid@gmail.com>
2023-09-29 08:10:12 +0530

    Add animation to contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,31 +1,52 @@
 import "./contact.css";
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function Contact() {
   return (
-    <div className="contact">
-      <div className="textContainer">
-        <h1>Let&apos;s work together</h1>
-        <div className="item">
+    <motion.div
+      className="contact"
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <motion.h1 variants={variants}>Let&apos;s work together</motion.h1>
+        <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
           <span>me@sahils.in</span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Address</h2>
           <span>India</span>
-        </div>
-        <div className="item">
+        </motion.div>
+        <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
           <span>+91 100</span>
-        </div>
-      </div>
-      <div className="formContainer">
+        </motion.div>
+      </motion.div>
+      <motion.div className="formContainer" variants={variants}>
         <form>
           <input type="text" required placeholder="Name" />
           <input type="email" required placeholder="Email" />
           <textarea rows={8} placeholder="Message" />
           <button>Submit</button>
         </form>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
